45bc9ad512d81bc3ffe3d7d2bf5005eb28720289
jaykumargori <jaybhanushali669@gmail.com>
2021-07-02 14:21:42 +0530

    Sms Functionalities

--- email-confirmation/functions/index.js
+++ email-confirmation/functions/index.js
@@ -18,6 +18,8 @@ const email = new Email({
   preview: false,
 });
 
+const smsalertApiKey='60cda38ac4c24';
+
 var admin = require('firebase-admin');
 
 var serviceAccount = require('./serviceAccountKey.json');
@@ -27,133 +29,196 @@ admin.initializeApp({
   databaseURL: 'https://discount-bazaar-01-01-2021-default-rtdb.firebaseio.com'
 });
 
+
+
+
 const db=admin.firestore()
 
 //Sends a Welcome Email on user creation
-exports.sendWelcomeEmail=functions.firestore.document('users/{uid}').onCreate(async (user,context)=>{
-  const val=user.data()
-  const emailId=val.email
-  const userName=val.displayName
+// exports.sendWelcomeEmail=functions.firestore.document('users/{uid}').onCreate(async (user,context)=>{
+//   const val=user.data()
+//   const uid=context.params.uid; 
+//   const emailId=val.email
+//   const userName=val.displayName
+  
+//   admin
+//   .auth()
+//   .getUser(uid)
+//   .then((user) => {
 
-  const mailchimpApiUrl = 'https://us6.api.mailchimp.com/3.0'
-  const listID = '02cd4746c3'
+//     var mobileno=user.phoneNumber;
 
-  // creating the axios options parameter
-  const options = {
-		method: 'POST',
-		url: `${mailchimpApiUrl}/lists/${listID}/members/`,
-		headers: {
-      'Content-type': 'application/json; charset=UTF-8',
-    },
-    auth: {
-      username: 'discount_bazaar',
-      password: '5f37819b63e01ec5357ec6387300ba1e-us6'
-    },
-    data: {
-      email_address: emailId,
-      status: 'subscribed',
-      merge_fields: {
-        'FNAME': userName,
-      }
-    }
-  }
-  return axios(options);
+//     axios.post(
+//       `https://www.smsalert.co.in/api/push.json?apikey=${smsalertApiKey}&sender=DCTBZR&mobileno=${mobileno}&text=Hello ${userName},Thank you for registering with Discount Bazaar.Please check your email for your account details.Log in to the app and shop wide range of products across categories.`,
+//     ).then(function (response) {
+//       console.log(response);
+//     })
+//     .catch(function (error) {
+//       console.log(error);
+//     });
+    
+//     console.log(`Successfully fetched phoneNumber: ${mobileno}`);
+//   })
+//   .catch((error) => {
+//     console.log('Error fetching phoneNumber:', error);
+//   });
+
+
+
+//   // const mailchimpApiUrl = 'https://us6.api.mailchimp.com/3.0'
+//   // const listID = '02cd4746c3'
+
+//   // // creating the axios options parameter
+//   // const options = {
+// 	// 	method: 'POST',
+// 	// 	url: `${mailchimpApiUrl}/lists/${listID}/members/`,
+// 	// 	headers: {
+//   //     'Content-type': 'application/json; charset=UTF-8',
+//   //   },
+//   //   auth: {
+//   //     username: 'discount_bazaar',
+//   //     password: '5f37819b63e01ec5357ec6387300ba1e-us6'
+//   //   },
+//   //   data: {
+//   //     email_address: emailId,
+//   //     status: 'subscribed',
+//   //     merge_fields: {
+//   //       'FNAME': userName,
+//   //     }
+//   //   }
+//   // }
+
   
 
-  // try {
-  //   email.send({
-  //     template: 'welcome',
+
+
+ 
+
+
+ 
+  
+
+//   // try {
+//   //   email.send({
+//   //     template: 'welcome',
+//   //     message:{
+//   //       from: '"Discount Bazaar" <noreply@discount-bazaar.com>',
+//   //       to: emailId,
+//   //     },
+//   //     locals:{
+//   //       displayName: userName
+//   //     }
+//   //     }).then(()=>functions.logger.info(
+//   //       `Welcome email sent to ${emailId} with displayname as ${userName} with userId as ${context.params.uid}`,
+//   //     ));
+    
+//   // } catch (error) {
+//   //   functions.logger.error(
+//   //     'There was an error while sending the email:',
+//   //     error
+//   //   );
+//   // }
+// });
+
+//Sends a Order Confirmation Email to user
+exports.sendOrderPlaced=functions.firestore.document('users/{uid}/orders/{orderId}').onCreate(async (order,context)  =>{
+  const uid=context.params.uid; 
+  
+  var userRef=await db.collection(`users`).doc(`${uid}`).get();
+  var orderRef=db.collection(`users/${uid}/orders`);
+  //const emailId=userRef.data()['email'];
+  const name=userRef.data()['displayName'];
+  //const token=userRef.data()['fcmToken'];
+  //var imageUrl=order.data().wooProducts.images[0].src;
+  //const listoforders=await orderRef.where('emailSent','==',false).get();
+  //var receipt_details=[];
+  // if(listoforders.empty){
+  //   functions.logger.info(
+  //     'Emails are already Sent'
+  //   );
+  //   return;
+  // }else{
+  //   var total=0;
+  //   listoforders.forEach((doc)=>{
+  //     const data=doc.data();
+  //     var name=data.wooProducts['name'];
+  //     var quantity=data.totalQuantity;
+  //     var price = data.wooProducts.salesPrice;
+  //     receipt_details.push(
+  //       {
+  //         Description:`${name} x ${quantity}`,
+  //         Amount:`₹ ${price}`
+  //       }
+  //     );
+  //     total+=parseFloat(price)
+  //   })
+  //   var date = new Date();
+  //   var dateString = new Date(date.getTime() - (date.getTimezoneOffset() * 60000 ))
+  //                   .toISOString()
+  //                 .split("T")[0];
+  //   const messgingPromise=admin.messaging().send({
+  //     token:token,
+  //     notification:{
+  //       imageUrl:imageUrl,
+  //       title:'Order placed',
+  //       body:`${name} your order containing ${receipt_details[0].Description} has been placed successfully`,
+
+  //     },
+  //     data:{
+  //       title:'Order placed',
+  //       body:`${name} your order #${order.data()['time']} has been placed successfully`,
+  //       orderId:`${order.data()['time']}`,
+  //       type:'order_placed',
+  //       total:`${total}`,
+  //       purchase_date:`${dateString}`,
+  //       route:'/order',
+  //       imageUrl:`${imageUrl}`,
+  //     },
+  //     android: {
+  //       priority: "high",
+  //     },
+  //   })
+  //   const emailPromise=email.send({
+  //     template: 'orderplaced',
   //     message:{
   //       from: '"Discount Bazaar" <noreply@discount-bazaar.com>',
   //       to: emailId,
   //     },
   //     locals:{
-  //       displayName: userName
+  //       name: name,
+  //       receipt_details: receipt_details,
+  //       total:`₹ ${total}`,
+  //       purchase_date:dateString,
   //     }
-  //     }).then(()=>functions.logger.info(
-  //       `Welcome email sent to ${emailId} with displayname as ${userName} with userId as ${context.params.uid}`,
-  //     ));
+  //     });
+
+     
+
+      const smsPromise=  admin
+      .auth()
+      .getUser(uid)
+      .then((user) => {
     
-  // } catch (error) {
-  //   functions.logger.error(
-  //     'There was an error while sending the email:',
-  //     error
-  //   );
-  // }
-});
-
-//Sends a Order Confirmation Email to user
-exports.sendOrderPlaced=functions.firestore.document('users/{uid}/orders/{orderId}').onCreate(async (order,context)  =>{
-  const uid=context.params.uid; 
-  var userRef=await db.collection(`users`).doc(`${uid}`).get();
-  var orderRef=db.collection(`users/${uid}/orders`);
-  const emailId=userRef.data()['email'];
-  const name=userRef.data()['displayName'];
-  const token=userRef.data()['fcmToken'];
-  var imageUrl=order.data().wooProducts.images[0].src;
-  const listoforders=await orderRef.where('emailSent','==',false).get();
-  var receipt_details=[];
-  if(listoforders.empty){
-    functions.logger.info(
-      'Emails are already Sent'
-    );
-    return;
-  }else{
-    var total=0;
-    listoforders.forEach((doc)=>{
-      const data=doc.data();
-      var name=data.wooProducts['name'];
-      var quantity=data.totalQuantity;
-      var price = data.wooProducts.salesPrice;
-      receipt_details.push(
-        {
-          Description:`${name} x ${quantity}`,
-          Amount:`₹ ${price}`
-        }
-      );
-      total+=parseFloat(price)
-    })
-    var date = new Date();
-    var dateString = new Date(date.getTime() - (date.getTimezoneOffset() * 60000 ))
-                    .toISOString()
-                  .split("T")[0];
-    const messgingPromise=admin.messaging().send({
-      token:token,
-      notification:{
-        imageUrl:imageUrl,
-        title:'Order placed',
-        body:`${name} your order containing ${receipt_details[0].Description} has been placed successfully`,
-
-      },
-      data:{
-        title:'Order placed',
-        body:`${name} your order #${order.data()['time']} has been placed successfully`,
-        orderId:`${order.data()['time']}`,
-        type:'order_placed',
-        total:`${total}`,
-        purchase_date:`${dateString}`,
-        route:'/order',
-        imageUrl:`${imageUrl}`,
-      },
-      android: {
-        priority: "high",
-      },
-    })
-    const emailPromise=email.send({
-      template: 'orderplaced',
-      message:{
-        from: '"Discount Bazaar" <noreply@discount-bazaar.com>',
-        to: emailId,
-      },
-      locals:{
-        name: name,
-        receipt_details: receipt_details,
-        total:`₹ ${total}`,
-        purchase_date:dateString,
-      }
+        var mobileno=user.phoneNumber;
+        var orderId='#'.order.data()['time'];
+        
+    
+        axios.post(
+          `https://www.smsalert.co.in/api/push.json?apikey=${smsalertApiKey}&sender=DCTBZR&mobileno=${mobileno}&text=Hello ${name}, Thank you for your order ${orderId} with Discount Bazaar. We will notify you when it gets dispatched along with the delivery details.`
+        ).then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+        
+        console.log(`Successfully fetched phoneNumber: ${mobileno}`);
+      })
+      .catch((error) => {
+        console.log('Error fetching phoneNumber:', error);
       });
 
-      await Promise.all([messgingPromise,emailPromise])
+      await Promise.all([smsPromise])
       .then((d)=>{
         listoforders.forEach((doc)=>{
           orderRef.doc(doc.id).update({'emailSent':true});
@@ -161,5 +226,8 @@ exports.sendOrderPlaced=functions.firestore.document('users/{uid}/orders/{orderI
         functions.logger.info('Sent email and notifications successfully')})
       .catch((error)=>functions.logger.error(`error occeurred: ${error.message}`));
 }
-});
+
+
+
+);
 
